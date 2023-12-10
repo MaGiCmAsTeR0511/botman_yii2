ca705b5c3dc0498b4e070170ab273986babc6c67
Kurt Hohenauer <kurt@hohenauers.eu>
2023-12-10 19:37:43 +0100

    Added some Bugfixes

--- config/db.php
+++ config/db.php
@@ -2,13 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=db;dbname=db',
-    'username' => 'root',
-    'password' => 'root',
+    'dsn' => 'mysql:host=192.168.178.24:3307;dbname=yii2basic',
+    'username' => 'mysql_admin',
+    'password' => ')kbj1xErya.pYCvd',
     'charset' => 'utf8',
-
-    // Schema cache options (for production environment)
-    //'enableSchemaCache' => true,
-    //'schemaCacheDuration' => 60,
-    //'schemaCache' => 'cache',
 ];
